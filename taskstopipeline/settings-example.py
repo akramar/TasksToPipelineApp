33b4f4e3f7d54c893825f9ebd94c3f83a2c693aa
Alain Kramar <alain.kramar@gmail.com>
2014-11-08 03:54:04 +0800

    Deleting settings-example.py
    Makes more sense to commit and then ignore settings.py instead.

--- taskstopipeline/settings-example.py
+++
@@ -1,8 +0,0 @@
-# Used by config.py, for oauth calls
-CLIENT_ID = 'app-id'
-CLIENT_SECRET = 'app-secret'
-
-# used internally for list crypt, etc
-CRYPTO_SALT = '0000000000000000'
-BLOCK_SIZE = 16
-PADDING = '{'
